7f1d8fc3abc550a83c3ba1c5875fba934ae83c39
Rahmonbek <83601261+Rahmonbek@users.noreply.github.com>
2022-04-08 10:09:55 +0500

    кепкеп

---
+++ .history/src/host_20220407175001.js
@@ -0,0 +1,9 @@
+export const url="https://qashqadaryo.vatanparvar.uz/Files/News"
+export const urlV="https://vatanparvar.uz/Files/News"
+export const api="https://qashqadaryo.vatanparvar.uz/"
+export const urlFoto="https://qashqadaryo.vatanparvar.uz/Files/Photogallery"
+export const urlStat="https://qashqadaryo.vatanparvar.uz/Files/StatisticElements"
+export const urlPer="https://qashqadaryo.vatanparvar.uz/Files/Personal"
+export const region=5
+export const regionI=1710
+export const urlImg="https://kadr.vatanparvar.uz/img/personal/"
